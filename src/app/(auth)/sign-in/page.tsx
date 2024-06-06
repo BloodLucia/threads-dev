5606d71a3ab85354be567a1e75fff88dffe5cf86
Dennis Koay <thecvcoder@foxmail.com>
2024-06-07 04:27:37 +0800

    fix:修复登录页面引入了被删除的组件

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,5 +1,3 @@
-import { NavBar } from "@/components/NavBar";
-
 export default function SignInPage() {
   return (
     <div className="min-h-screen flex flex-col">
@@ -7,10 +5,24 @@ export default function SignInPage() {
       <main className="w-full min-h-[calc(100vh-(60px+70px))] shrink-0 overflow-y-auto flex justify-center items-center">
         <form className="w-full max-w-[370px] px-[24px] flex flex-col items-stretch gap-2">
           <div className="text-center text-2xl font-semibold mb-5">Threads</div>
-          <input autoComplete="username" className="bg-[rgb(245,245,245)] p-[16px] outline-none rounded-xl placeholder:text-[rgb(184,184,184)]" type="text" placeholder="账号或邮箱" />
-          <input autoComplete="username" className="bg-[rgb(245,245,245)] p-[16px] outline-none rounded-xl placeholder:text-[rgb(184,184,184)]" type="text" placeholder="账号或邮箱" />
+          <input
+            autoComplete="username"
+            className="bg-[rgb(245,245,245)] p-[16px] outline-none rounded-xl placeholder:text-[rgb(184,184,184)]"
+            type="text"
+            placeholder="账号或邮箱"
+          />
+          <input
+            autoComplete="username"
+            className="bg-[rgb(245,245,245)] p-[16px] outline-none rounded-xl placeholder:text-[rgb(184,184,184)]"
+            type="text"
+            placeholder="账号或邮箱"
+          />
           {/* <input type="password" placeholder="密码" /> */}
-          <input type="button" value="登录" className="mt-5 bg-black text-white h-[48px] rounded-xl" />
+          <input
+            type="button"
+            value="登录"
+            className="mt-5 bg-black text-white h-[48px] rounded-xl"
+          />
         </form>
       </main>
       <footer className="h-[70px] k"></footer>
